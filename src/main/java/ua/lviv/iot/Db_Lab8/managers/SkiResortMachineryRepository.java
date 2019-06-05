8e7e85508439b04bab920455543b0cbb8cca50f3
victoriafolyush <victoriafolyush@gmail.com>
2019-06-05 21:59:40 +0300

    added lab8

---
+++ src/main/java/ua/lviv/iot/Db_Lab8/managers/SkiResortMachineryRepository.java
@@ -0,0 +1,7 @@
+package ua.lviv.iot.Db_Lab8.managers;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SkiResortMachineryRepository  extends CrudRepository<SkiResortMachinery, Integer>{
+
+}
---
+++ src/test/java/ua/lviv/iot/Db_Lab8/DbLab8ApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.lviv.iot.Db_Lab8;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DbLab8ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
